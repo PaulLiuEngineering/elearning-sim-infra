dd8339992be0b65ef695df98fdf44809dc99035f
Paul <paulliu14@gmail.com>
2026-04-06 17:09:10 -0400

    alb and stuff

--- terraform/live/aws/prod/ap-east-1/alb/outputs.tf
+++ terraform/live/aws/prod/ap-east-1/alb/outputs.tf
@@ -1,21 +1,31 @@
 output "alb_arn" {
   description = "ARN of the production ALB."
-  value       = aws_lb.this.arn
+  value       = module.alb.alb_arn
 }
 
 output "alb_dns_name" {
   description = "DNS name of the production ALB."
-  value       = aws_lb.this.dns_name
+  value       = module.alb.alb_dns_name
 }
 
 output "alb_zone_id" {
   description = "Hosted zone ID of the production ALB."
-  value       = aws_lb.this.zone_id
+  value       = module.alb.alb_zone_id
 }
 
 output "acm_certificate_arn" {
   description = "ARN of the ACM certificate for the Lumio Learning domains."
-  value       = aws_acm_certificate.this.arn
+  value       = module.acm.requested_certificate_arn
+}
+
+output "cn_acm_certificate_arn" {
+  description = "ARN of the separate ACM certificate for the .cn domains."
+  value       = module.acm_cn.requested_certificate_arn
+}
+
+output "cn_certificate_validation_records" {
+  description = "DNS validation records to add manually in Aliyun for the .cn certificate."
+  value       = module.acm_cn.domain_validation_options
 }
 
 output "hosted_zone_id" {
@@ -25,5 +35,5 @@ output "hosted_zone_id" {
 
 output "target_group_arn" {
   description = "ARN of the placeholder target group for future ECS attachment."
-  value       = aws_lb_target_group.placeholder.arn
+  value       = module.alb.target_group_arn
 }
