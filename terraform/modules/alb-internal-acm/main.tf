59e4f321b8ca762e3e81589fe427420a57cae452
Paul <paulliu14@gmail.com>
2026-04-09 18:31:38 -0400

    massive refactor

--- terraform/modules/alb-internal-acm/main.tf
+++ terraform/modules/alb-internal-acm/main.tf
@@ -1,3 +1,14 @@
+data "aws_ssm_parameter" "allowed_cidr_blocks" {
+  for_each = toset(var.allowed_cidr_block_ssm_parameter_names)
+
+  name            = each.value
+  with_decryption = false
+}
+
+locals {
+  effective_allowed_cidr_blocks = length(var.allowed_cidr_block_ssm_parameter_names) > 0 ? sort(distinct(compact([for parameter in data.aws_ssm_parameter.allowed_cidr_blocks : trimspace(parameter.value)]))) : var.allowed_cidr_blocks
+}
+
 module "acm" {
   source = "../acm"
 
@@ -12,7 +23,7 @@ module "alb" {
   name_prefix         = var.name_prefix
   vpc_id              = var.vpc_id
   subnet_ids          = var.subnet_ids
-  allowed_cidr_blocks = var.allowed_cidr_blocks
+  allowed_cidr_blocks = local.effective_allowed_cidr_blocks
   certificate_arn     = module.acm.certificate_arn
   tags                = var.tags
 }
